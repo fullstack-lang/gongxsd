e13fd208ad8926779010df06619abba505999157
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2024-08-19 17:52:58 +0200

    generated books

--- test/books/go/models/models.go
+++ test/books/go/models/models.go
@@ -12,7 +12,7 @@ type A_books struct {
 	// insertion point for fields
 
 	// generated from element "book" of type bookType order 8 depth 1
-	Book []*BookType `xml:"book"`
+	Book []*BookType `xml:"book,omitempty"`
 }
 
 // BookType is generated from named complex type "bookType"
@@ -24,11 +24,11 @@ type BookType struct {
 	// generated from attribute group "extendedAttributes
 	AttributeGroup_extendedAttributes
 
-	// generated from group 
+	// generated from group with order 0 depth 0
 	Group_bookDetailsGroup
 
 	// generated from element "credit" of type credit order 1 depth 0
-	Credit []*Credit `xml:"credit"`
+	Credit []*Credit `xml:"credit,omitempty"`
 }
 
 // Credit is generated from named complex type "credit"
@@ -41,16 +41,16 @@ type Credit struct {
 	Page int `xml:"page,attr,omitempty"`
 
 	// generated from element "credit-type" of type xs:string order 9 depth 0
-	Credit_type string `xml:"credit-type"`
+	Credit_type string `xml:"credit-type,omitempty"`
 
 	// generated from element "link" of type link order 13 depth 0
-	Link []*Link `xml:"link"`
+	Link []*Link `xml:"link,omitempty"`
 
 	// generated from element "credit-words" of type xs:string order 14 depth 0
-	Credit_words string `xml:"credit-words"`
+	Credit_words string `xml:"credit-words,omitempty"`
 
 	// generated from element "credit-symbol" of type xs:string order 15 depth 0
-	Credit_symbol string `xml:"credit-symbol"`
+	Credit_symbol string `xml:"credit-symbol,omitempty"`
 }
 
 // Link is generated from named complex type "link"
@@ -72,16 +72,16 @@ type Group_bookDetailsGroup struct {
 	// insertion point for fields
 
 	// generated from element "title" of type titleType order 3 depth 1
-	Title string `xml:"title"`
+	Title string `xml:"title,omitempty"`
 
 	// generated from element "author" of type xs:string order 4 depth 1
-	Author string `xml:"author"`
+	Author string `xml:"author,omitempty"`
 
 	// generated from element "year" of type yearType order 5 depth 1
-	Year int `xml:"year"`
+	Year int `xml:"year,omitempty"`
 
 	// generated from element "format" of type bookFormatEnum order 6 depth 1
-	Format string `xml:"format"`
+	Format string `xml:"format,omitempty"`
 }
 
 // AttributeGroup_commonAttributes is generated from named attribute group "commonAttributes"
