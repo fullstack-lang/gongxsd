2928501811c85beff98b4116d7d625ff06242e91
peugeot <thomaspeugeot@yahoo.fr>
2024-10-25 08:22:00 +0200

    new gongc

--- go/orm/back_repo.go
+++ go/orm/back_repo.go
@@ -13,7 +13,9 @@ import (
 	"github.com/fullstack-lang/gongxsd/go/db"
 	"github.com/fullstack-lang/gongxsd/go/models"
 
+	/* THIS IS REMOVED BY GONG COMPILER IF TARGET IS gorm
 	"github.com/fullstack-lang/gongxsd/go/orm/dbgorm"
+	THIS IS REMOVED BY GONG COMPILER IF TARGET IS gorm */
 
 	"github.com/tealeg/xlsx/v3"
 )
@@ -88,10 +90,9 @@ func NewBackRepo(stage *models.StageStruct, filename string) (backRepo *BackRepo
 
 	var db db.DBInterface
 
-	/* THIS IS REMOVED BY GONG COMPILER IF TARGET IS lite
 	db = NewDBLite()
-	THIS IS REMOVED BY GONG COMPILER IF TARGET IS lite */
 
+	/* THIS IS REMOVED BY GONG COMPILER IF TARGET IS gorm
 	db = dbgorm.NewDBWrapper(filename, "github_com_fullstack_lang_gongxsd_go",
 		&AllDB{},
 		&AnnotationDB{},
@@ -120,6 +121,7 @@ func NewBackRepo(stage *models.StageStruct, filename string) (backRepo *BackRepo
 		&UnionDB{},
 		&WhiteSpaceDB{},
 	)
+	THIS IS REMOVED BY GONG COMPILER IF TARGET IS gorm */
 
 	backRepo = new(BackRepoStruct)
 
