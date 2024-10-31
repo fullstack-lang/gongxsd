8116968c56b1335ae906da2bf1ead088c3845907
peugeot <thomaspeugeot@yahoo.fr>
2024-10-31 19:45:23 +0100

    new gongc

--- go/controllers/register_controllers.go
+++ go/controllers/register_controllers.go
@@ -309,7 +309,7 @@ func (controller *Controller) onWebSocketRequestForBackRepoContent(c *gin.Contex
 			// ReadMessage is used to detect client disconnection
 			_, _, err := wsConnection.ReadMessage()
 			if err != nil {
-				log.Println("github.com/fullstack-lang/gongxsd/go", stackPath, "WebSocket read error (client disconnected):", err)
+				log.Println("github.com/fullstack-lang/gongxsd/go", stackPath, "WS client disconnected:", err)
 				cancel() // Cancel the context
 				return
 			}
@@ -345,11 +345,13 @@ func (controller *Controller) onWebSocketRequestForBackRepoContent(c *gin.Contex
 				// Send backRepo data
 				err = wsConnection.WriteJSON(backRepoData)
 				if err != nil {
-					log.Println("github.com/fullstack-lang/gongxsd/go:\n",
-						"client no longer receiver web socket message, assuming it is no longer alive, closing websocket handler")
+					log.Println("github.com/fullstack-lang/gongxsd/go:\n", stackPath,
+						"client no longer receiver web socket message,closing websocket handler")
 					fmt.Println(err)
 					cancel() // Cancel the context
 					return
+				} else {
+					log.Println("github.com/fullstack-lang/gongxsd/go:", stackPath, "sent backRepoData")
 				}
 			}
 		}
