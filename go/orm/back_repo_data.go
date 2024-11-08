f164cd20666442c4bcb22d0075e07362fcab41c6
peugeot <thomaspeugeot@yahoo.fr>
2024-11-08 08:18:31 +0100

    new gongc & go get -u ./...

--- go/orm/back_repo_data.go
+++ go/orm/back_repo_data.go
@@ -58,6 +58,11 @@ type BackRepoData struct {
 }
 
 func CopyBackRepoToBackRepoData(backRepo *BackRepoStruct, backRepoData *BackRepoData) {
+
+	// wait till backRepo is written by commit
+	backRepo.rwMutex.RLock()
+	defer backRepo.rwMutex.RUnlock()
+
 	// insertion point for slices copies
 	for _, allDB := range backRepo.BackRepoAll.Map_AllDBID_AllDB {
 
